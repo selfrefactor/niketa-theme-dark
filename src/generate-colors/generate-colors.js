fafa1e8d706467bf6d087ea7b582123c8c3d1a72
Deyan Totev <deyan8284@gmail.com>
2024-07-22 20:20:11 +0300

    feat: test lint out

--- src/generate-colors/generate-colors.js
+++ src/generate-colors/generate-colors.js
@@ -4,6 +4,7 @@ const { writeJson } = require('fs-extra')
 
 function generateColors({ input, levels = 20 }) {
   const [first, second] = input
+  const a = 12
   const OUTPUT = `${__dirname}/_COLORS.json`
 
   const colors = piped(getGradient(first, second, levels), uniq)
